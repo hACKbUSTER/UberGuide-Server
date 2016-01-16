78573e2714eac34737b17f8ac91ea284a6e02b17
misasagi <kosmos@misasagideMacBook-Air.local>
2016-01-17 04:45:43 +0800

    init map

---
+++ CloudCode/uber/map.js
@@ -0,0 +1,23 @@
+module.exports = function(req,res){
+    if(library.inputChecker(req.params,["request_id"])){
+        return res.error(
+            library.stdIntError("require params [request_id]")
+        );
+    }
+    var request_id = req.params.request_id;
+
+    library.getAccessToken(config.client_id).then(function(user){
+        return _Get(
+            CONST.UBER_API
+                      + "/v1/requests/"
+                      + request_id
+                      + "/map",{},{
+                          "Authorization":" Bearer "
+                              + user.get('access_token')
+                      });
+    }).then(function(body){
+        return res.success(library.stdReturn(body));
+    }).catch(function(e){
+        return res.error(library.stdIntError(e));
+    });
+};
