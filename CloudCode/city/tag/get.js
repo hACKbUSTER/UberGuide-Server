6887abfaa23b6c869922e9d60bfc1ff6db7e0c5f
Angela Wang <angelawang@raventech.com>
2016-01-17 09:51:45 +0800

    git add tag sort order

--- CloudCode/city/tag/get.js
+++ CloudCode/city/tag/get.js
@@ -1,6 +1,6 @@
 module.exports = function(request, response) {
 	var query = new AV.Query(ACL.Tag);
-	
+	query.addAscending('order');
 	query.find({
 		success : function(results) {
 			response.success(library.stdReturn(results));
